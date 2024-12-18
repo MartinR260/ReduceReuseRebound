2d324e3a84b7cee3acc59e13ed666fc46bde3153
MartinR260 <martinkata2006@gmail.com>
2024-12-18 09:24:36 +0200

    Added New Assets

--- Player.gd
+++ Player.gd
@@ -1,10 +1,8 @@
 extends CharacterBody2D
 
+@export var SPEED = 130.0
+@export var JUMP_VELOCITY = -200.0
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -200.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var double_jump = true;
 var air_time = 1;
@@ -22,8 +20,9 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * air_time * (delta / 2)
 		air_time += delta
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and double_jump:
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or double_jump):
 		double_jump = false;
 		air_time /= 1.5
 		velocity.y = JUMP_VELOCITY
@@ -32,16 +31,8 @@ func _physics_process(delta):
 			velocity.y = max(-80, velocity.y)
 		
 	if is_on_floor():
-		double_jump = true;
+		double_jump = false; #Make 'true' for double jump.
 		air_time = 1
-	
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#apply_friction()
 
 	move_and_slide()
 
