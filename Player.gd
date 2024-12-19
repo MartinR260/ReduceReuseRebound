df7e72449f3680d85f8b47604fbcdb15605eb59b
MartinR260 <martinkata2006@gmail.com>
2024-12-19 03:24:06 +0200

    Added Animations

--- Player.gd
+++ Player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
+@onready var sprite = $AnimatedSprite2D
 @export var SPEED = 130.0
-@export var JUMP_VELOCITY = -200.0
+@export var JUMP_VELOCITY = -270.0 #200
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var double_jump = true;
@@ -14,25 +15,38 @@ func _physics_process(delta):
 	if input.x == 0:
 		apply_friction()
 	else:
+		if input.x < 0:
+			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
+		sprite.animation = "Walk"
+		sprite.play()
 		apply_acceleration(input.x)
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * air_time * (delta / 2)
+		#velocity.y += gravity * air_time * (delta / 2)
+		velocity.y += gravity * delta
 		air_time += delta
+		sprite.animation = "Jump"
+		sprite.play()
+		
 		
 	# Handle jump.
-	if Input.is_action_pressed("ui_accept") and (is_on_floor() or double_jump):
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or double_jump):
 		double_jump = false;
 		air_time /= 1.5
 		velocity.y = JUMP_VELOCITY
 	else:
 		if Input.is_action_just_released("ui_accept"):
-			velocity.y = max(-80, velocity.y)
+			velocity.y = max(-80, velocity.y) #-80
 		
 	if is_on_floor():
 		double_jump = false; #Make 'true' for double jump.
 		air_time = 1
+		if velocity.x == 0:
+			sprite.animation = "Idle"
+			sprite.play()
 
 	move_and_slide()
 
