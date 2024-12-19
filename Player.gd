a84ad015274abcc331d0409aa5168c26f0255e2d
MartinR260 <martinkata2006@gmai.com>
2024-12-19 17:31:42 +0200

    Added Aiming

--- Player.gd
+++ Player.gd
@@ -2,23 +2,34 @@ extends CharacterBody2D
 
 @onready var sprite = $AnimatedSprite2D
 @export var SPEED = 130.0
-@export var JUMP_VELOCITY = -270.0 #200
+@export var JUMP_VELOCITY = -260.0 #200
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var double_jump = true;
 var air_time = 1;
+var idle_timer = 0.0
+var last_mouse_position = Vector2.ZERO
 
 func _physics_process(delta):
+	var mouse_position = get_global_mouse_position()
+	var direction = mouse_position - global_position
+
 	var input = Vector2.ZERO
-	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	
+	if direction.x < 0:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+		
 	if input.x == 0:
 		apply_friction()
+		
 	else:
-		if input.x < 0:
-			sprite.flip_h = true
-		else:
-			sprite.flip_h = false
+		#if input.x < 0:
+			#sprite.flip_h = true
+		#else:
+			#sprite.flip_h = false
 		if is_on_wall():
 			sprite.animation = "Still"
 			sprite.play()
@@ -47,14 +58,39 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 	else:
 		if Input.is_action_just_released("ui_accept"):
-			velocity.y = max(-80, velocity.y) #-80
+			velocity.y = max(-80, velocity.y)
+		
+		
+	if mouse_position != last_mouse_position:
+		idle_timer = 0.0
+		last_mouse_position = mouse_position
+	else:
+		idle_timer += delta
 		
 	if is_on_floor():
 		double_jump = false; #Make 'true' for double jump.
 		air_time = 1
 		if velocity.x == 0:
-			sprite.animation = "Idle"
-			sprite.play()
+			if idle_timer > 1.5:
+				sprite.animation = "Idle"
+				sprite.play()
+			else:
+				if (direction.angle() > -0.3 and direction.angle() < -0.15) or (direction.angle() > -2.8 and direction.angle() < -2.65):
+					sprite.animation = "AimUpStill"
+					sprite.play()
+				if direction.angle() > -2.65 and direction.angle() < -0.3:
+					sprite.animation = "AimAboveStill"
+					sprite.play()
+				if (direction.angle() < 0.3 and direction.angle() > 0.15) or (direction.angle() < 2.8 and direction.angle() > 2.65):
+					sprite.animation = "AimDownStill"
+					sprite.play()
+				if direction.angle() < 2.65 and direction.angle() > 0.3:
+					sprite.animation = "AimUnderStill"
+					sprite.play()
+				if (direction.angle() > -0.15 and direction.angle() < 0.15) or direction.angle() < -2.8 or direction.angle() > 2.8:
+					sprite.animation = "Still"
+					sprite.play()
+		
 
 	move_and_slide()
 
